f360399366af11f5529fb26920df35ce6efe5df5
gilvanjuniorskyward <gilvanjunior@icloud.com>
2026-04-07 15:27:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -256,8 +256,8 @@ def favorite(id):
 # -------------------------
 
 with app.app_context():
+    db.drop_all()
     db.create_all()
-
 # -------------------------
 # RUN LOCAL
 # -------------------------
